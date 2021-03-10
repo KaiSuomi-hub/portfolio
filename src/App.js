04b69f83fc86e7692121d69d9acf5ef53c3c47e4
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2021-03-10 15:41:58 +0200

    first rev of frontend, no content yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'jquery';
+import '@popperjs/core'; 
+import Content from './Content';
+import Navi from './Navi'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="navbar-fixed-left">
+          <Navi/>
+        <div className="row">
+          <div id="me">.</div>
+      <Content
+        // Left={"This is on the Left side"}
+            Para={ <text >Hi<br />
+                    My name is Antti Suomi and I'm an entry level code monkey.
+                        <br />
+              <br />
+              I love pretty things and I'm a bit slow so I mainly mess around with front end side of things.
+              <br /><br />
+              Take a look below for some stuff I've done with React, Bootstrap and CSS
+            </text>
+            }
+          // Right={"This is on the right"} 
+          />
+          <div id="code">.</div>
+          <Content
+        // Left={"This is on the Left side"}
+            Para={ <text ><br />
+              This is the code side
+              <br />
+              <br />
+              ss
+              <br /><br />
+              ss
+            </text>
+            }
+          // Right={"This is on the right"} 
+          />
+                    <div id="cv">.</div>
+
+            <Content
+        // Left={"This is on the Left side"}
+            Para={ <text ><br />
+              This is the CVVVV
+              <br />
+              <br />
+              ss
+              <br /><br />
+              ss
+            </text>
+            }
+          // Right={"This is on the right"} 
+        />
+        </div>
+      </div>
+      </div>
+
   );
 }
 
 export default App;
+
+ //<a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
---
+++ src/Navi.js
@@ -0,0 +1,26 @@
+
+import Face from './Face';
+import  React  from 'react';
+
+class Navi extends React.Component{
+    render() {
+        return (
+            
+            <nav role="navigation">
+            <ul>
+              <li><a href="#"><Face/></a>
+                <ul class="dropdown">
+                  
+                  <li id="link"><a href="#me">Me</a></li>
+                  <li id="link"><a href="#code">Code</a></li>
+                    <li id="link"><a href="#cv">Cv</a></li>
+              
+                </ul>
+              </li>
+            </ul>
+          </nav>
+                
+   );
+}
+}
+export default Navi;
